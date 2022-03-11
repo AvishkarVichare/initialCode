d18a16d795e892abb670d2b4603ab73ef754e6a1
AvishkarVichare <avie9500@gmail.com>
2022-03-12 01:52:46 +0530

    commited 2nd time

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
 const express = require('express');
 const app = express();
-const  bodyParser = require('body-parser')
 const knex =require('./knex')
+const PORT = 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
-const db = require('./functions')
 
 
-app.post('/',async(req,res)=>{
+// adding route 
+app.post('/api/add',async(req,res)=>{
    
+    try{
+
     let added = await knex('infos').insert(req.body);
+    res.json(added)
 
-    
-    console.log(added)
-res.json(added)
+}    catch{
+   
+    res.json({"err":"Error occured"})
+}
 
 })
 
-app.get("/get",async(req,res)=>{
-    let info = await knex("infos").select("*");
+// getting route 
+app.get("/api/get",async(req,res)=>{
+    try{
+        let info = await knex("infos").select("*");
+        res.json(info)
+    }
+    catch{
+
+        res.status(404).json({"err":"Not Found"})
+    }
     
-    res.json(info)
 })
 
-app.listen(8000,()=>{
+
+app.listen(PORT,()=>{
     console.log("running boy");
 })
